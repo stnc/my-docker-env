8cf28366ba10e1acea8d3c1c856893ad5d9ac265
Stnc mAC <selmantunc@yandex.com>
2023-08-01 10:57:51 -0600

    add

---
+++ go/golang-postresql-volume/scripts/pq-host.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+
+cd /data/postgres
+echo "host all all all $POSTGRES_HOST_AUTH_METHOD" >> pg_hba.conf
+# FILE_CERT_NAME=servercert
+# if [ -f "certificates/$FILE_CERT_NAME.crt" ] && [ -f "$FILE_CERT_NAME.key" ] ; then
+#     echo "Cert and Key already exist"
+# else 
+#     echo "Cert and Key does not exist, trying to create new ones..."
+#     apk update && apk add openssl && rm -rf /var/cache/apk/*
+#     openssl req -new -subj "/C=US/ST=California/CN=localhost" \
+#         -newkey rsa:2048 -nodes -keyout "$FILE_CERT_NAME.key" -out "$FILE_CERT_NAME.csr"
+#     openssl x509 -req -days 365 -in "$FILE_CERT_NAME.csr" -signkey "$FILE_CERT_NAME.key" -out "certificates/$FILE_CERT_NAME.crt" -extfile "self-signed-cert.ext"
+# fi
